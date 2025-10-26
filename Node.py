05fb613c7f7c8b17d45b7c66cb2f2ee4259b84a6
JennifM98 <jennifermorales.2942@gmail.com>
2025-10-26 00:14:26 -0400

    Node class with inordertraversal, search node, and insert to left and right tree

---
+++ Node.py
@@ -0,0 +1,60 @@
+from Token import *
+#Node Class
+class Node:
+    def __init__(self, Data):
+        self.data= Data
+        self.left= None
+        self.right= None
+
+
+#Inorder traversal from the parent node
+def inorder(node):
+    if node == None:
+        return
+    
+    inorder(node.left)
+    print(node.data)
+    inorder(node.right)
+
+#searchs for node and returns the node
+def dfs(node, searchNode):
+    if node == None:
+        return None
+    
+    if node.data == searchNode:
+        return node
+    
+    searchLeft = dfs(node.left, searchNode)
+    if searchLeft != None:
+        return searchLeft
+
+    searchRight = dfs(node.right, searchNode)
+    if searchRight != None:
+        return searchRight
+
+
+def insertLeft(node, newNode):
+    if node.left == None:
+        node.left = newNode
+
+def insertRight(node, newNode):
+    if node.right == None:
+        node.right = newNode
+
+"""
+if __name__ == "__main__":
+    root = Node(2)
+    insertLeft(root, Node(3))
+    insertRight(root, Node(4))
+    insertLeft(dfs(root, 4), Node(5))
+    insertRight(dfs(root, 4), Node(6))
+
+    inorder(root)
+
+
+    root2= Node(Token("Identifier", "x"))
+    insertLeft(root2, Node(Token("Identifier", "y")))
+
+    inorder(root2)
+
+"""
