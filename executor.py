185a140eb48e3a73824d7ee842878b46fc65b4d7
Alex Tidwell <altidwell97@gmail.com>
2025-11-21 16:45:40 -0500

    minor alterations

--- executor.py
+++ executor.py
@@ -2,7 +2,7 @@ from Node import *
 
 # This method will hold the main loop that goes through our parse tree and calls
 # other methods
-def exectutor(trees):
+# def exectutor(trees):
 
 # This takes the tree for an import and translates it to it's equivalent in C
 def imports(tree):
@@ -11,4 +11,5 @@ def imports(tree):
     return translated_line
 
 # We need a method for the starting keywords function, define, display
-# (one that handles with variables and one for without, or two options in the same method), and set
+# (one that handles with variables and one for without, or two options in the same method), set,
+# exit, and endfun
